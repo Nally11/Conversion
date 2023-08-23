79621a018d94edac69b60bf782124e11d922d9fd
Nally11 <mythk2@hotmail.com>
2023-08-23 14:32:56 +0200

    primary testing

---
+++ currency_test.py
@@ -0,0 +1,46 @@
+import requests
+
+def convert_usd_to_gbp(amount):
+    base_url = "http://data.fixer.io/api/latest"
+    access_key = "d7bd455444c7f747bd569d2c59426a19"  
+    params = {
+        "access_key": access_key,
+        "symbols": "GBP",
+        "base": "USD"
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        gbp_rate = data["rates"]["GBP"]
+        gbp_amount = amount * gbp_rate
+        return gbp_amount
+    else:
+        print("Failed to fetch data from Fixer.io")
+        return None
+
+# Test the function
+usd_amount = 100
+gbp_equivalent = convert_usd_to_gbp(usd_amount)
+
+
+#if gbp_equivalent is not None:
+ #   print(f"{usd_amount} USD is equal to {gbp_equivalent:.2f} GBP")
+
+
+response = requests.get(base_url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)  # Print the entire response JSON
+    gbp_rate = data.get("rates", {}).get("GBP")
+    if gbp_rate is not None:
+        gbp_amount = amount * gbp_rate
+        return gbp_amount
+    else:
+        print("GBP rate not found in response")
+        return None
+else:
+    print("Failed to fetch data from Fixer.io")
+    return None
---
+++ test.py
@@ -0,0 +1,32 @@
+def cm_to_inch(cm):
+    return cm / 2.54
+
+def inch_to_cm(inch):
+    return inch * 2.54
+
+def meters_to_feet(meters):
+    return meters * 3.281
+
+def feet_to_meters(feet):
+    return feet / 3.281
+
+def liters_to_gallons(liters):
+    return liters * 0.264172
+
+def gallons_to_liters(gallons):
+    return gallons / 0.264172
+
+# Test the functions
+cm = 100
+inch = 10
+meters = 5
+feet = 16
+liters = 10
+gallons = 2.5
+
+print(f"{cm} cm is equal to {cm_to_inch(cm):.2f} inches")
+print(f"{inch} inches is equal to {inch_to_cm(inch):.2f} cm")
+print(f"{meters} meters is equal to {meters_to_feet(meters):.2f} feet")
+print(f"{feet} feet is equal to {feet_to_meters(feet):.2f} meters")
+print(f"{liters} liters is equal to {liters_to_gallons(liters):.2f} gallons")
+print(f"{gallons} gallons is equal to {gallons_to_liters(gallons):.2f} liters")
